e7c92af61aa014eca08cb282fc13cf649a808f9a
mark antony <markworld223@gmail.com>
2017-06-19 12:13:42 +0000

    added profile models and db

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619074646) do
+ActiveRecord::Schema.define(version: 20170619120332) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "profiles", force: true do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "relationship_status"
+    t.string   "hometown"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
