08ae61d330a1f7a596694295a2a6d63575680bbe
mark antony <markworld223@gmail.com>
2017-06-21 08:00:03 +0000

    modified profile area

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :only_current_user
+    
     def new
         @user = User.find( params[:user_id] )
         @profile = Profile.new
@@ -15,9 +18,28 @@ class ProfilesController < ApplicationController
         end
     end
     
+    def edit
+        @user = User.find( params[:user_id] )
+        @profile = @user.profile
+    end
     
+    def update
+      @user = User.find( params[:user_id] )
+      @profile = @user.profile
+      if @profile.update_attributes(profile_params)
+          redirect_to user_path( params[:user_id] )
+          flash[:success] = "Profile Updated"
+      else 
+          render action: :edit 
+      end
+    end
     
     private
+     def only_current_user
+         @user = User.find( params[:user_id] )
+         redirect_to(root_url) unless @user == current_user 
+     end
+     
      def profile_params
       params.require(:profile).permit(:first_name, :last_name, :hometown, :relationship_status, :description)
      end
